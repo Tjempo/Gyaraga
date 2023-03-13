fc1959badf9d39871f2543344674dcef261f2835
Tjempo <79266628+Tjempo@users.noreply.github.com>
2023-03-13 09:43:38 +0100

    Edit Files
    Dingen aangepast zodat het nu van ons is
    thxsm voor de code, heheheh.

---
+++ src/main/java/com/github/hanyaeger/Gyaraga/Gyaraga.java
@@ -0,0 +1,27 @@
+package com.github.hanyaeger.Gyaraga;
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.YaegerGame;
+//import com.github.hanyaeger.Gyaraga.scenes.GameLevel;
+//import com.github.hanyaeger.Gyaraga.scenes.EndScreen;
+
+
+public class Gyaraga extends YaegerGame {
+    public static void main(String[] args){
+        launch(args);
+
+    }
+
+    @Override
+    public void setupGame() {
+        setGameTitle("Gyaraga");
+        setSize(new Size(500, 800));
+    }
+
+    @Override
+    public void setupScenes() {
+//        addScene(0, new TitleScene(this));
+//        addScene(1, new GameLevel());
+//        addScene(2, new EndScreen(this));
+    }
+
+}
