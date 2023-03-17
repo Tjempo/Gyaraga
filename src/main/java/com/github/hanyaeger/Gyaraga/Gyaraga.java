b83c44cf89fda1f39f3e3ca653598e10b83c2624
Tjempo <79266628+Tjempo@users.noreply.github.com>
2023-03-17 14:29:36 +0100

    Buttons++

--- src/main/java/com/github/hanyaeger/Gyaraga/Gyaraga.java
+++ src/main/java/com/github/hanyaeger/Gyaraga/Gyaraga.java
@@ -1,14 +1,13 @@
 package com.github.hanyaeger.Gyaraga;
 import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.YaegerGame;
-//import com.github.hanyaeger.Gyaraga.scenes.GameLevel;
-//import com.github.hanyaeger.Gyaraga.scenes.EndScreen;
+import com.github.hanyaeger.Gyaraga.scenes.LevelSelect;
+
 
 
 public class Gyaraga extends YaegerGame {
     public static void main(String[] args){
         launch(args);
-        //balls
     }
 
     @Override
@@ -19,7 +18,7 @@ public class Gyaraga extends YaegerGame {
 
     @Override
     public void setupScenes() {
-//        addScene(0, new TitleScene(this));
+        addScene(0, new LevelSelect(this));
 //        addScene(1, new GameLevel());
 //        addScene(2, new EndScreen(this));
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,6 @@ module waterworld {
     opens audio;
     opens backgrounds;
     opens sprites;
+    exports com.github.hanyaeger.Gyaraga.entities.buttons;
+    exports com.github.hanyaeger.Gyaraga.scenes;
 }
